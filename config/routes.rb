f818a70055d7df6bdced322a3120b5b86f85e338
Heather Lee <heather.k.lee@gmail.com>
2018-06-26 19:39:28 -0400

    use namespacing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # STEP 1: A ROUTE triggers a controller action
   # verb "/urls" => "namespace/controllers#action"
-  get "/contact" => "api/contacts#get_contact"
-  get "/allcontacts" => "api/contacts#get_all"
+  namespace :api do
+      get "/contact" => "contacts#get_contact"
+      get "/allcontacts" => "contacts#get_all"
+    end
   #Api::ContactsController#get_all is missing a template for this request format and variant.<-- read later
 end
