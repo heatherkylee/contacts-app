9afd51ee65aad39adc83dc950b8f867e9c7c4c15
Heather Lee <heather.k.lee@gmail.com>
2018-08-13 23:31:53 -0400

    create contacts index and new page

--- public/index.js
+++ public/index.js
@@ -87,12 +87,73 @@ var LogoutPage = {
   }
 };
 
+var ContactsPage = {
+  template: "#contacts-page",
+  data: function() {
+    return {
+      message: "Contacts!",
+      contacts: []
+    };
+  },
+  created: function() {
+    axios.get("/api/contacts").then(function(response) {
+      console.log(response.data);
+      this.contacts = response.data;
+    }.bind(this));
+  },
+  methods: {},
+  computed: {}
+};
+
+var ContactsNewPage = {
+  template: "#contacts-new-page",
+  data: function() {
+    return {
+      message: "Add a New Contact",
+      contact: {
+        firstName: "",
+        lastName: "",
+        email: "",
+        phoneNumber: "",
+        middleName: "",
+        bio: ""
+      },
+      errors: []
+    };
+  },
+  methods: {
+    submit: function() {
+      var params = {
+        input_first_name: this.contact.firstName,
+        input_last_name: this.contact.lastName,
+        input_email: this.contact.email,
+        input_phone_number: this.contact.phone_number,
+        input_middle_name: this.contact.middleName,
+        input_bio: this.contact.bio
+      };
+      axios
+        .post("/api/contacts", params)
+        .then(function(response) {
+          console.log(response);
+          router.push("/contacts");
+        })
+        .catch(
+          function(error) {
+            this.errors = error.response.data.errors;
+          }.bind(this)
+        );
+    }
+  }
+};
+
 var router = new VueRouter({
   routes: [
     { path: "/", component: HomePage },
     { path: "/signup", component: SignupPage },
     { path: "/login", component: LoginPage },
-    { path: "/logout", component: LogoutPage }
+    { path: "/logout", component: LogoutPage },
+    { path: "/contacts", component: ContactsPage },
+    { path: "/contacts/new", component: ContactsNewPage }
   ],
   scrollBehavior: function(to, from, savedPosition) {
     return { x: 0, y: 0 };
